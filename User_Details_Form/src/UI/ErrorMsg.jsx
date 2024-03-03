be0ee8c8d4b7008502a44900818c3dbf3b2d91b6
CoolShivas <shivas2710cool00@gmail.com>
2024-03-03 09:59:16 +0530

    JSX_limitation_And_Solution

--- User_Details_Form/src/UI/ErrorMsg.jsx
+++ User_Details_Form/src/UI/ErrorMsg.jsx
@@ -1,23 +1,48 @@
+import { createPortal } from 'react-dom'
+import ReactDOM from "react-dom/client";
+import React from "react";
 import classes from "./ErrorMsg.module.css";
 import Card from "./Card";
 import Button from "./Button";
 
+
+const Backdrop = ({ errorHandlerBtnABC }) => {
+    return <div className={classes.backdrop} onClick={errorHandlerBtnABC}></div>
+}
+
+const ModalOverlay = ({ titleABC, messageABC, errorHandlerBtnABC }) => {
+    return <Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>
+                {titleABC}
+            </h2>
+        </header>
+        <main className={classes.content}>
+            {messageABC}
+        </main>
+        <footer className={classes.actions}>
+            <Button onClick={errorHandlerBtnABC}> Okay </Button>
+        </footer>
+    </Card>
+}
+
+
 const ErrorMsg = ({ titleABC, messageABC, errorHandlerBtnABC }) => {
-    return <div className={classes.backdrop} >
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>
-                    {titleABC}
-                </h2>
-            </header>
-            <main className={classes.content}>
-                {messageABC}
-            </main>
-            <footer className={classes.actions}>
-                <Button onClick={errorHandlerBtnABC}> Okay </Button>
-            </footer>
-        </Card>
-    </div>
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClick={errorHandlerBtnABC} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={titleABC}
+                    message={messageABC}
+                    onClick={errorHandlerBtnABC}
+                />
+            )}
+        </React.Fragment>
+    )
 }
 
 export default ErrorMsg;
